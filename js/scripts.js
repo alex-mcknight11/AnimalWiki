3519360750709a351220c81d3bbb4ca48c4f2004
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-13 10:26:48 -0800

    WIP dropdown function
    Jack Jones <jacktjones4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,40 @@
-$(document).ready(function(){
-  const animalOptions = $("#animalOptions").val();
-$(#animalOptions).submit(function(event) {
+$(document).ready(function()
 
-
-
-
-
-    $("#animalOptions").click(function() {
-      $("#animalOptions").hide();
-    });
-
-    if (".Raccoon") {
-      $('#wolfFact', "#bearFact").hide();
-    });
-  });
+{
+  if (value="Raccoon") {
+  $('Raccoon').show();
+} else {
+  $('Bear').hide();
+}
 });
 
 
+//   $("#formAnimals").submit(function() {
+//     event.preventDefault();
+//     const animalOptions = $("#animalOptions").val();
+
+//     if ("#Raccoon") {
+//       $('#raccoonFact').show();
+//       $('#wolfFact', #'bearFact').hide();
+//     } else {
+//       $('#Bear')
+//     }
+//     }
+//   });
+
+
+//     $(#animalOptions).submit(function(event) {
+
+
+
+
+
+//     $("#animalOptions").click(function() {
+//       $("#animalOptions").hide();
+//     });
+
+//     if (".Raccoon") {
+//       $('#wolfFact', "#bearFact").hide();
+//     });
+//   });
+// });
