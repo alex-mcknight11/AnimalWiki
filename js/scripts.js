29aa186dbab829bcb3b976dc4a09cb6589fb0bd4
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-13 10:01:58 -0800

    create and establish animal wiki
    Jack Jones <jacktjones4@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  const animalOptions = $("#animalOptions").val();
+$(#animalOptions).submit(function(event) {
+
+
+
+
+
+    $("#animalOptions").click(function() {
+      $("#animalOptions").hide();
+    });
+
+    if (".Raccoon") {
+      $('#wolfFact', "#bearFact").hide();
+    });
+  });
+});
+
+
